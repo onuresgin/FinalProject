e8d2107432e12d8621fd629d1fd4d6b3b0d6674c
Onur Esgin <onur_esgin_1905@hotmail.com>
2021-02-11 00:04:24 +0300

    10 şubat ders sonu

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager()
+        {
+        }
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public IEnumerable<object> CategoryName { get; set; }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _categoryDal.Get(c => c.CategoryId == categoryId);
+        }
+    }
+}
