d5d24ef291258d35edd725492c3dc348959eb74f
Onur Esgin <onur_esgin_1905@hotmail.com>
2021-02-25 00:09:24 +0300

    24 şubat ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -15,21 +15,21 @@ namespace ConsoleUI
 
         private static void CategoryTest()
         {
-            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll())
+            //CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            //foreach (var category in categoryManager.GetAll())
             {
-                Console.WriteLine(category.CategoryName);
+                //Console.WriteLine(category.CategoryName);
             }
         }
 
         private static void ProductTest()
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+           // ProductManager productManager = new ProductManager(new EfProductDal());
             
 
-            foreach (var product in productManager.GetProductDetails().Data)
+            //foreach (var product in productManager.GetProductDetails().Data)
             {
-                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                //Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
         }
     }
